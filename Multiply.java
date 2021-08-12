145c890a2b15d8cbff2c4cbe3c301c62b87df314
AB-creator <anushreebhandarwar35@gmail.com>
2021-08-12 19:15:45 +0530

    added Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -1,9 +1,9 @@
-public class DIff {
+public class Multiply {
     public static void main(String[] args) {
         int a=5;
         int b=6;
 
-        System.out.println("The difference is" + (a-b));
+        System.out.println("The product is" + (a*b));
         
     }
     

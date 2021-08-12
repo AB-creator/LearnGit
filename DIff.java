6b9ae8db983aac35025c122759b42aa1b3f03806
AB-creator <anushreebhandarwar35@gmail.com>
2021-08-12 20:56:40 +0530

    added int d i DIff.java

--- DIff.java
+++ DIff.java
@@ -2,6 +2,7 @@ public class DIff {
     public static void main(String[] args) {
         int a=5;
         int b=6;
+        int d;
 
         System.out.println("The difference is" + (a-b));
         
